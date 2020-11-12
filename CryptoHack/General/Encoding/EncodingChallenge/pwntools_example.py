4dde7ac4774aa23ac541219af40028340209abdf
Ash <ashrafali379@gmail.com>
2020-11-12 09:33:17 +0000

    Finished Encoding

--- CryptoHack/General/Encoding/EncodingChallenge/pwntools_example.py
+++ CryptoHack/General/Encoding/EncodingChallenge/pwntools_example.py
@@ -1,5 +1,8 @@
 from pwn import *  # pip install pwntools
 import json
+import base64
+import codecs
+from Crypto.Util.number import bytes_to_long, long_to_bytes
 
 r = remote('socket.cryptohack.org', 13377, level='debug')
 
@@ -14,16 +17,48 @@ def json_send(hsh):
     r.sendline(request)
 
 
-received = json_recv()
+def decode_base64(enc):
+    return base64.b64decode(enc).decode()
 
-print("Received type: ")
-print(received["type"])
-print("Received encoded value: ")
-print(received["encoded"])
 
-to_send = {
-    "decoded": "changeme"
-}
-json_send(to_send)
+def decode_hex(enc):
+    return unhex(enc).decode()
+
+
+def decode_rot13(enc):
+    return codecs.decode(enc, 'rot_13')
+
+
+def decode_bigint(enc):
+    return long_to_bytes(int(enc, 16)).decode()
+
+
+def decode_utf(enc):
+    return "".join(chr(o) for o in enc)
+
+
+for i in range(100):
+    received = json_recv()
+
+    print("Received type: ")
+    print(received["type"])
+    print("Received encoded value: ")
+    print(received["encoded"])
+    message = ""
+    if received["type"] == 'base64':
+        message = decode_base64(received["encoded"])
+    if received["type"] == 'hex':
+        message = decode_hex(received["encoded"])
+    if received["type"] == 'rot13':
+        message = decode_rot13(received["encoded"])
+    if received["type"] == 'bigint':
+        message = decode_bigint(received["encoded"])
+    if received["type"] == 'utf-8':
+        message = decode_utf(received["encoded"])
+    print(f"Message: {message}")
+    to_send = {
+        "decoded": message
+    }
+    json_send(to_send)
 
 json_recv()
